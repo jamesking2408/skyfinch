3f0ada27f6e28016dc7aeede1e337aa2ed4883f0
jamesking2408 <134200425+jamesking2408@users.noreply.github.com>
2023-07-27 15:20:08 +0530

    chatbox

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
     "use strict";
 
     // Spinner
-    var spinner = function() {
-        setTimeout(function() {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();  
+    // var spinner = function() {
+    //     setTimeout(function() {
+    //         if ($('#spinner').length > 0) {
+    //             $('#spinner').removeClass('show');
+    //         }
+    //     }, 3);
+    // };
+    // spinner();  
+
+    // New spinner
+
+
+
 
 
     // Initiate the wowjs
@@ -208,12 +213,120 @@ function screenCheck() {
         })
         //<!-- OwlCarousel script end -->
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//<!-- Chat BOX Design Start -->
+window.onload = () => {
+    document.getElementById("txt-cursor").focus;
+}
+
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+
+
+let userMessage;
+
+const responses = [
+    "How can I help you?",
+    "I'm doing great, thank you!",
+    "Please ask me something else.",
+    "I'm still learning. Can you be more specific?",
+    "Tell me more about that!",
+    "can you please wait...?",
+    "I'm your friend! feel free ask me..."
+  ];
 
 
 
+const createChatLi = (message, className) =>{
+    // Create a chat <li> element with passed message and className
+    const chatli = document.createElement("li");
+    chatli.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>`:`<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    chatli.innerHTML = chatContent;
+    return chatli;
+}
+
+const getRandomResponse = () => {
+    const randomIndex = Math.floor(Math.random()*responses.length);
+    return responses[randomIndex];
+}
 
 
 
+const handleChat = () =>{
+    userMessage = chatInput.value.trim();
+    // console.log(userMessage);
+    if(!userMessage) return;
+
+    // Append the user's message to the chatbox 
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        let botResponse ;
+        if((userMessage.toLowerCase() === "hi") || (userMessage.toLowerCase() === "hi skyfinch") ){
+        botResponse = "Hi 🖐 Nice to meet you.";
+        }
+        else if(userMessage.toLowerCase() === "hello"){
+        botResponse = "Hello! Nice to meet you.";
+        }
+        else if(userMessage.toLowerCase() === "good morning"){
+        botResponse = "Good Morning Have a Nice day!";
+        }
+        else if(userMessage.toLowerCase() === "thank you"){
+            botResponse = "I'm doing great, thank you!";
+        }
+        else if(userMessage.toLowerCase() === "welcome"){
+            botResponse = "Thats's intresting. It's my pleasure.";
+        }
+        else if(userMessage.toLowerCase() === "bye"){
+        botResponse = "Bye! Nice to meet you.";
+        }
+        else if(userMessage.toLowerCase() === "help"){
+        botResponse = "<b>You may have ask me like one of the question.</b> <br>1.Who is CEO? <br>2.In Which year skyfinch established? <br>3.What are the Service?<br>4.Contact Details?<br>";
+        }
+        else if((userMessage.toLowerCase() === 'who is ceo?')||(userMessage.toLowerCase() === 'who is ceo')|| (userMessage.toLowerCase() === 'company ceo?')||(userMessage.toLowerCase() === 'ceo?')||(userMessage.toLowerCase() === 'ceo')){
+            botResponse = "<b>Kabilan Sokkalingam - Founder</b> <br>Kabilan Sokkalingam - Founder, Managing Partner, CEO & President.";
+            }
+        else if((userMessage.toLowerCase() === 'what is your age?')|| (userMessage.toLowerCase() === 'your age?')||(userMessage.toLowerCase() === 'skyfinch starting year?')||(userMessage.toLowerCase() === 'in which year skyfinch established')||(userMessage.toLowerCase() === 'in which year skyfinch established?')){
+        botResponse = "business professionals having more than 20 years of experience in various business domains. Since its establishment in the year 2004 at Puducherry, India";
+        }
+        else if((userMessage.toLowerCase() === 'what are the service?')||(userMessage.toLowerCase() === 'service')||(userMessage.toLowerCase() === 'service?')){
+        botResponse = "1. Software development <br>2. Web Application <br> 3. Mobile Application development<br>4. Email<br>5. SMS <br>6.Payment Integration.</br>";
+        }
+        else if((userMessage.toLowerCase() === "contact details?")||(userMessage.toLowerCase() === "contact details")||(userMessage.toLowerCase() === "how can i contact you?")){
+            botResponse = "<b>Address :</b> No.5, Republic Street, Reddiarpalayam, Puducherry -605 010, India. <br> <b> Ph :</b> +91 413-4207 333, 94425 90611, 94434 83240, 94899 18298 <br> <b>E-mail :</b> info@skyfinch.com";
+            }
+        else{
+            botResponse = getRandomResponse();
+        }
+        
+        // message thinking while waiting for the response
+        const incomingChatLi = createChatLi(botResponse, "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+    }, 2000);
+
+    // Clear the input after sending the message
+    chatInput.value = '';
+}
+
+// Function to handle Enter key press
+const handleKeyPress = (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        handleChat();
+    }
+}
+
+sendChatBtn.addEventListener("click", handleChat)
+
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
+chatInput.addEventListener("keypress", handleKeyPress);
 
 
 
