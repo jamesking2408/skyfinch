b1c93cc3f4a08c531ab2c099f5bfa0370ad49e32
jamesking2408 <134200425+jamesking2408@users.noreply.github.com>
2023-09-14 19:25:59 +0530

    update

--- js/chatbox.js
+++ js/chatbox.js
@@ -123,22 +123,42 @@
 
     //<!-- Chat BOX JS End -->
 
-    // <!-- shuffle button start -->
-  // JavaScript to handle button clicks
-  document.getElementById('lbt').addEventListener('click', function() {
-    let checkedItem = document.querySelector('.cards1 input:checked');
-    let prevItem = checkedItem.previousElementSibling;
-    if (prevItem) {
-      checkedItem.checked = false;
-      prevItem.checked = true;
-    }
-  });
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
-  document.getElementById('rbt').addEventListener('click', function() {
-    let checkedItem = document.querySelector('.cards1 input:checked');
-    let nextItem = checkedItem.nextElementSibling;
-    if (nextItem) {
-      checkedItem.checked = false;
-      nextItem.checked = true;
+
+    // <!-- shuffle button start -->
+  // JavaScript to handle button clicks / Button click move to right or left
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the radio buttons and buttons
+    const radioButtons = document.querySelectorAll('input[name="slider"]');
+    const leftButton = document.getElementById('left-button');
+    const rightButton = document.getElementById('right-button');
+
+    // Function to go to the next slide
+    function nextSlide() {
+        const checkedIndex = Array.from(radioButtons).findIndex(input => input.checked);
+        const nextIndex = (checkedIndex + 1) % radioButtons.length;
+        radioButtons[nextIndex].checked = true;
     }
-  });
+
+    // Function to go to the previous slide
+    function previousSlide() {
+        const checkedIndex = Array.from(radioButtons).findIndex(input => input.checked);
+        const previousIndex = (checkedIndex - 1 + radioButtons.length) % radioButtons.length;
+        radioButtons[previousIndex].checked = true;
+    }
+
+    function changeBg() {
+        document.querySelector(".bg-00").classList.toggle('blue');
+    }
+
+    // Add event listeners to the buttons
+    leftButton.addEventListener('click', previousSlide);
+    rightButton.addEventListener('click', nextSlide);
+    
+    leftButton.addEventListener('click', changeBg);
+    rightButton.addEventListener('click', changeBg);
+
+
+});
